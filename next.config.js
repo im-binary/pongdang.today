25eb095e26cc002ef9f192ede510d80e6ddc0959
pongdang <pongpong0121@kakao.com>
2022-10-10 18:07:08 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -5,58 +5,28 @@ const nextConfig = {
   rewrites: () => {
     return [
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "pongdang.world",
-          },
-        ],
-        destination: "https://pongdang.world/:path*",
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "blog.pongdang.today",
-          },
-        ],
-        destination: "https://blog.pongdang.today/:path*",
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "diary.pongdang.today",
-          },
-        ],
-        destination: "https://diary.pongdang.today/:path*",
-      },
-      {
-        source: "/blog",
-        destination: "https://pongdang.world/",
-      },
-      {
-        source: "/blog/:path*",
-        destination: "https://pongdang.world/:path*",
-      },
-      {
-        source: "/vanilla-blog",
-        destination: "https://blog.pongdang.today/",
-      },
-      {
-        source: "/vanilla-blog/:path*",
-        destination: "https://blog.pongdang.today/:path*",
-      },
-      {
         source: "/diary",
         destination: "https://diary.pongdang.today/diary",
       },
       {
         source: "/diary/:path*",
         destination: "https://diary.pongdang.today/diary/:path*",
+      },
+      {
+        source: "/blog",
+        destination: "https://pongdang-world.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://pongdang-world.vercel.app/blog/:path*",
+      },
+      {
+        source: "/vanilla-blog",
+        destination: "https://blog.pongdang.today/vanilla-blog",
+      },
+      {
+        source: "/vanilla-blog/:path*",
+        destination: "https://blog.pongdang.today/vanilla-blog/:path*",
       },
     ];
   },
